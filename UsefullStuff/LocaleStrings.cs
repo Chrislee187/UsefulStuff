9ce3747275653ae595d221229013b94ae80d5cc5
Chris Lee <chrislee187@gmail.com>
2015-08-30 12:32:00 +0100

    #codelove

--- UsefullStuff/LocaleStrings.cs
+++ UsefullStuff/LocaleStrings.cs
@@ -10,7 +10,56 @@ namespace UsefulStuff
     /// </summary>
     public static class LocaleStrings
     {
-        private enum  User32StringIds : uint
+        public static string Close { get; }
+        public static string Yes { get; }
+        public static string TryAgain { get; }
+        public static string Retry { get; }
+        public static string Ok { get; }
+        public static string No { get; }
+        public static string Ignore { get; }
+        public static string Help { get; }
+        public static string Continue { get; }
+        public static string Cancel { get; }
+        public static string Abort { get; }
+        public static string Error { get; }
+
+        public static string Load(uint id) => LoadString(id);
+
+        static LocaleStrings()
+        {
+            _libraryHandle = GetModuleHandle(Environment.SystemDirectory + "\\User32.dll");
+            if (_libraryHandle == IntPtr.Zero) throw new FileLoadException("Unable to get User32.dll Module Handle");
+            Error = LoadString((uint)User32StringIds.Error);
+            Abort = LoadString((uint)User32StringIds.Abort);
+            Cancel = LoadString((uint)User32StringIds.Cancel);
+            Close = LoadString((uint)User32StringIds.Close);
+            Continue = LoadString((uint)User32StringIds.Continue);
+            Help = LoadString((uint)User32StringIds.Help);
+            Ignore = LoadString((uint)User32StringIds.Ignore);
+            No = LoadString((uint)User32StringIds.No);
+            Ok = LoadString((uint)User32StringIds.Ok);
+            Retry = LoadString((uint)User32StringIds.Retry);
+            TryAgain = LoadString((uint)User32StringIds.TryAgain);
+            Yes = LoadString((uint)User32StringIds.Yes);
+        }
+
+        static string LoadString(uint Ident)
+        {
+            var sb = new StringBuilder(1024);
+
+            return LoadString(_libraryHandle, Ident, sb, 1024) > 0 
+                ? sb.ToString().Replace("&","") 
+                : string.Empty;
+        }
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern int LoadString(IntPtr hInstance, uint uID, StringBuilder lpBuffer, int nBufferMax);
+
+        private static IntPtr _libraryHandle;
+        private enum User32StringIds : uint
         {
             Error = 2,
             Ok = 800,
@@ -37,59 +86,5 @@ namespace UsefulStuff
             //            Other = 1004,
             //            Unknown = 8267,
         }
-
-        public static string Close { get; }
-        public static string Yes { get; }
-        public static string TryAgain { get; }
-        public static string Retry { get; }
-        public static string Ok { get; }
-        public static string No { get; }
-        public static string Ignore { get; }
-        public static string Help { get; }
-        public static string Continue { get; }
-        public static string Cancel { get; }
-        public static string Abort { get; }
-        public static string Error { get; }
-
-        private static IntPtr _libraryHandle;
-
-        static LocaleStrings()
-        {
-            _libraryHandle = GetModuleHandle(Environment.SystemDirectory + "\\User32.dll");
-            if (_libraryHandle == IntPtr.Zero) throw new FileLoadException("Unable to get User32.dll Module Handle");
-            Error = LoadString((uint)User32StringIds.Error);
-            Abort = LoadString((uint)User32StringIds.Abort);
-            Cancel = LoadString((uint)User32StringIds.Cancel);
-            Close = LoadString((uint)User32StringIds.Close);
-            Continue = LoadString((uint)User32StringIds.Continue);
-            Help = LoadString((uint)User32StringIds.Help);
-            Ignore = LoadString((uint)User32StringIds.Ignore);
-            No = LoadString((uint)User32StringIds.No);
-            Ok = LoadString((uint)User32StringIds.Ok);
-            Retry = LoadString((uint)User32StringIds.Retry);
-            TryAgain = LoadString((uint)User32StringIds.TryAgain);
-            Yes = LoadString((uint)User32StringIds.Yes);
-        }
-
-        static string LoadString(uint Ident)
-        {
-
-            StringBuilder sb = new StringBuilder(1024);
-            int size = LoadString(_libraryHandle, Ident, sb, 1024);
-            if (size > 0)
-                return sb.ToString().Replace("&","");
-
-            return string.Empty;
-        }
-
-        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
-        private static extern IntPtr GetModuleHandle(string lpModuleName);
-
-        [DllImport("user32.dll", CharSet = CharSet.Auto)]
-        private static extern int LoadString(IntPtr hInstance, uint uID, StringBuilder lpBuffer, int nBufferMax);
-
-
-        public static string Load(uint id) => LoadString(id);
-
     }
 }
