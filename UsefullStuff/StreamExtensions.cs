da8d3e9013bfc0b028e77b72e5d739446fcd8de8
Chris Lee <chrislee187@gmail.com>
2015-09-03 19:19:01 +0100

    Added .ToArray() Stream extension method

--- UsefullStuff/StreamExtensions.cs
+++ UsefullStuff/StreamExtensions.cs
@@ -19,5 +19,20 @@ namespace UsefulStuff
 
             return (byte)@byte;
         }
+        public static byte[] ToArray(this Stream stream)
+        {
+            byte[] buffer = new byte[32768];
+            using (MemoryStream ms = new MemoryStream())
+            {
+                while (true)
+                {
+                    int read = stream.Read(buffer, 0, buffer.Length);
+                    if (read <= 0)
+                        return ms.ToArray();
+                    ms.Write(buffer, 0, read);
+                }
+            }
+        }
     }
+
 }
